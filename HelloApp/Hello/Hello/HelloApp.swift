18eb263cfea4ec922bfe18bbd427cd80f5139216
chagana1 <chaganajay@gmail.com>
2026-08-23 11:19:10 -0700

    changes

---
+++ HelloApp/Hello/Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by cisstudent on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
